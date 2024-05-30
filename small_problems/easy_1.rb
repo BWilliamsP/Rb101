7b8e1a21434bfa5858b24255d7dc442735e21833
Ben Williams <benjaminwilliamssda@gmail.com>
2024-05-30 11:32:38 -1000

    finished first problem

---
+++ small_problems/easy_1.rb
@@ -0,0 +1,13 @@
+#Write a method that takes two arguments, a string and a positive integer, and prints the string as many times as the integer indicates.
+
+def repeat(words, num)
+    if num <= 0
+        puts "Please only positive integers"
+    else 
+        num.times do
+            puts words
+        end
+    end
+end
+
+repeat("Hey guys!",6)
